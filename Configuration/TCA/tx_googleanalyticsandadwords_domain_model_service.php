87baa0bf49748f0e340d12d47c96f9140cb976e1
Matt <matt.krilanovich@adswerve.com>
2021-06-08 08:27:51 -0700

    copied over prod extension from local containing logic for conversions

--- Configuration/TCA/tx_googleanalyticsandadwords_domain_model_service.php
+++ Configuration/TCA/tx_googleanalyticsandadwords_domain_model_service.php
@@ -8,6 +8,7 @@ return [
     'columns' => [
         'product' => [
             'label' => 'LLL:EXT:google_analytics_and_adwords/Resources/Private/Language/locallang_db.xlf:tx_googleanalyticsandadwords_domain_model_service.product',
+            'onChange' => 'reload',
             'config' => [
                 'type' => 'radio',
                 'default' => 'google-analytics',
@@ -16,17 +17,46 @@ return [
                     ['Google Ads', 'google-ads'],
                 ],
             ],
+            
+        ],
+        'ga_version' => [
+            'label' => 'LLL:EXT:google_analytics_and_adwords/Resources/Private/Language/locallang_db.xlf:tx_googleanalyticsandadwords_domain_model_service.ga_version',
+            'description' => 'Which version of Google Analytics?',
+            'config' => [
+                'type' => 'radio',
+                'default' => 'ga4',
+                'items' => [
+                    ['Google Analytics 4', 'ga4'],
+                    ['Universal Analytics', 'ua'],
+                ],
+            ],
+            'displayCond' => 'FIELD:product:=:google-analytics',
         ],
         'service_id' => [
             'label' => 'LLL:EXT:google_analytics_and_adwords/Resources/Private/Language/locallang_db.xlf:tx_googleanalyticsandadwords_domain_model_service.service_id',
+            'description' => 'Google Analytics 4 (G-XXXXXXXXX)
+            Universal Analytics (UA-XXXXXXXXX)
+            Google Ads (AW-XXXXXXXXX)',
             'config' => [
                 'type' => 'input',
                 'size' => '20',
                 'eval' => 'trim',
             ],
         ],
+        'conversion_label' => [
+            'label' => 'LLL:EXT:google_analytics_and_adwords/Resources/Private/Language/locallang_db.xlf:tx_googleanalyticsandadwords_domain_model_service.conversion_label',
+            'description' => 'What is the conversion label?',
+            'default' => 'CONVERSION_LABEL',
+            'config' => [
+                'type' => 'input',
+                'size' => '20',
+                'eval' => 'trim',
+            ],
+            'displayCond' => 'FIELD:product:=:google-ads',
+        ],
+
     ],
     'types' => [
-        '0' => ['showitem' => 'product, service_id'],
+        '0' => ['showitem' => 'product, ga_version, service_id, conversion_label'],
     ],
 ];
